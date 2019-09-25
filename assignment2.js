f62c4489bb4e8024d098415c7afae3ce6ccf15f3
Annelisa Moody <am@apeiron.io>
2019-09-24 18:01:35 -0700

    pseudocode the solution

---
+++ assignment2.js
@@ -0,0 +1,14 @@
+// Create a function that converts Farenheight to Celsius. 
+// Takes a single argument which represents degrees in Farenheight.
+// Converts the argument and retunrs the degrees in celsius.
+
+// Create another function that converts Celsius to Farenheight.
+// Takes an argument in Celsius and returns the degrees in Farenheight.
+
+// Implement convert(isFtoC, from, to).
+// If isFtoC = true, degrees must be converted from Farenheight to Celsius.
+// If isFtoC = false, degrees must be converted from Celcius to Farenheight.
+
+//Function must convert every degree in the specified range and show each degree and the converted result on the page. 
+
+
