cda8f9cf4d7d2b8a9bee9829e2725298fba7e77e
Annelisa Moody <am@apeiron.io>
2019-09-30 23:13:55 -0700

    fixing solution

--- assignment2.js
+++ assignment2.js
@@ -1,51 +1,46 @@
 // Create a function that converts Farenheight to Celsius. 
-
 function convertFtoC(fDeg) {
+    // takes the input that's typed into "from" on the HTML page
     fDeg = document.getElementById("from").value;
+
+    // finds the "output" id and writes the f to c conversion number
     document.getElementById("output").innerHTML = ((fDeg-32)/1.8).toFixed(2);  
 };
 
+//same thing as above but with C to F.
 function convertCtoF(cDeg) {
     cDeg = document.getElementById("from").value;
     document.getElementById("output").innerHTML = ((cDeg*1.8)+32).toFixed(2);
 };
 
+// finds the option that you selected from the dropdown 
 function getOption() {
     selectElement = document.querySelector('#convert');
+
+    // if the selected option is F to C, value = celsius. it performs the FtoC function
     if (selectElement.value == 'celsius') {
+        isFtoC == true;
         convertFtoC(fDeg);
+    // same but reverse
     } else if (selectElement.value == 'fahrenheit') {
+        isFtoC == false;
         convertCtoF(cDeg);
     }
 }
 
 
-// Takes a single argument which represents degrees in Farenheight.
-// Converts the argument and returns the degrees in celsius.
-
-// Create another function that converts Celsius to Farenheight.
-// Takes an argument in Celsius and returns the degrees in Farenheight.
-
-// Implement convert(isFtoC, from, to).
-// If isFtoC = true, degrees must be converted from Farenheight to Celsius.
-// If isFtoC = false, degrees must be converted from Celcius to Farenheight.
-
-//Function must convert every degree in the specified range and show each degree and the converted result on the page. 
-
-//loop "from" until "to";
-
-// let from = parseInt(document.getElementById('from').value); 
-// let to = parseInt(document.getElementById('to').value); 
+let from = parseInt(document.getElementById('from').value); 
+let to = parseInt(document.getElementById('to').value); 
+let isFtoC = true;
 
 
-// function convert(isFtoC, from, to) {
-//     if (document.getElementById('convert').value == "celsius") {
-//         let isFtoC = true;
-//         convertFtoC(from, to);
-
-//     }
-//     else {
-//         let isFtoC = false;
-//     }
-// }
-
+function convert(isFtoC, from, to) {
+    if (document.getElementById('convert').value == "celsius") {
+        let isFtoC = true;
+        convertFtoC(from, to);
+    }
+    else {
+        let isFtoC = false;
+        convertCtoF(from, to);
+    }
+}
